87ce3967c544f777c2aeabf3c16061bf74df8888
Julie Park <jspark721@gmail.com>
2016-04-07 12:47:58 -0700

    Defined a different root route for signed in users

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
 
+  authenticated :user do
+    root 'users#show', as: :authenticated_root
+  end
+  
   root 'welcome#index'
 end
