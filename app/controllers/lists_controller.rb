46c77a058ec99b0a035656dd646b085357806510
Julie Park <jspark721@gmail.com>
2016-04-08 16:14:23 -0700

    Added to-do list new and show view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,34 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
 
   def index
   end
-  
+
+  def show
+  end
+
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+
+    if @list.save
+      flash[:notice] = "To-do list was saved."
+      redirect_to @list
+    else
+      flash.now[:alert] = "There was an error saving the To-do list. Please try again."
+      render :new
+    end
   end
+
+  private
+    def set_list
+      @list = List.find(params[:id])
+    end
+
+    def list_params
+      params.require(:list).permit(:title, :description)
+    end
 end
