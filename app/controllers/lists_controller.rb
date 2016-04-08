3fd1e962bec6b0a2d1bff6086a7d3fedc486e37f
Julie Park <jspark721@gmail.com>
2016-04-08 15:00:14 -0700

    Created todo list controller and model

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,4 @@
+class ListController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'list/create'
+
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :lists
+  end
 
   authenticated :user do
     root 'users#show', as: :authenticated_root
