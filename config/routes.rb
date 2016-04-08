f1f7702cef21f30961ec2a13882647b9572b8f4b
Julie Park <jspark721@gmail.com>
2016-04-08 00:20:52 -0700

    Edited homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users, only: [:show]
 
   authenticated :user do
     root 'users#show', as: :authenticated_root
   end
-  
+
   root 'welcome#index'
 end
