0b89c7157442ee1d8847a505324d27fa84a0ffcf
Julie Park <jspark721@gmail.com>
2016-04-08 15:26:25 -0700

    Created lists controller and list model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :users, only: [:show]
+  resources :lists
 
   authenticated :user do
     root 'users#show', as: :authenticated_root
