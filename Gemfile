029b30d50f9c615170c244e802a8b7c945e80826
Julie Park <jspark721@gmail.com>
2016-04-22 01:30:43 -0500

    Installed serializer gem

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'figaro'
 gem 'pry'
 gem 'devise'
 gem 'faker'
+gem 'whenever', :require => false
+gem 'active_model_serializers'
 
 group :development, :test do
   gem 'byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :lists
+
+  def serialize
+    hash = {}
+    attributes.each { |key, val| hash[key] = val }
+    hash
 end
