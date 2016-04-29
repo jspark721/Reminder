5c1b087db8feeb89436360a193266eb11ccc4045
Julie Park <jspark721@gmail.com>
2016-04-29 13:56:40 -0500

    Added api routes and created list and item controllers

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,9 +15,23 @@ class Api::UsersController < ApiController
     render json: user
   end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:name,:password)
+  end
 
   def conditions_met
     true
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,15 @@ Rails.application.routes.draw do
   end
 
   namespace :api, defaults: { format: :json } do
-    resources :users
+    resources :users do
+      resources :lists
+    end
+
+    resources :lists, only: [] do
+      resources :items, only: [:create]
+    end
+
+    resources :items, only: [:destroy]
   end
 
   authenticated :user do
